56a7f32dec6754f34bc065593c082ac0f6e54bb6
DavideGit991 <pasto.davide1991@gmail.com>
2020-12-09 17:13:46 +0100

    aggiustamenti al layout finale

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// inserisco dinamicamente i link della main-nav attraverso Vue
+
+var app=new Vue({
+  el:"#app",
+
+  data:{
+
+    arrayLink:[
+      {
+        nome:"Home"
+      },
+      {
+        nome:"Who We Are"
+      },
+      {
+        nome:"What We Do"
+      },
+      {
+        nome:"Where We Work"
+      },
+      {
+        nome:"Carrers"
+      },
+      {
+        nome:"News"
+      },
+      {
+        nome:"Contact"
+      }
+    ]
+  }
+
+
+
+
+
+});
