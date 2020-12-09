6ef16b3547ac2b252ea7da9d02935ecf92329087
DavideGit991 <pasto.davide1991@gmail.com>
2020-12-09 21:25:26 +0100

    inserimento dinamico dell 'header e footer tramite vue

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ var app=new Vue({
 
   data:{
     active: false,
-    arraySocial:[
-
-    ],
+    descrizioneNav:"Avada & Associates legal team announce deal to open up oil & gas ",
+    logo:"img/avada-law-logo.png",
+    arraySocial:["fab fa-facebook-square","fab fa-twitter-square","fab fa-instagram","fab fa-youtube-square"],
     arrayLink:[
       {
         nome:"Home"
@@ -30,8 +30,11 @@ var app=new Vue({
       {
         nome:"Contact"
       }
-    ]
+    ],
+    numeroTel:"1.800.555.6789",
+    infoFooter:"© Copyright 2012 - 2020   |   Avada Theme by Theme Fusion   |   All Rights Reserved   |   Powered by WordPress"
   },
+
 
 // funzione per far scomparire la mini-nav allo scroll della pagina bind class riga 17 39 html
  mounted(){
